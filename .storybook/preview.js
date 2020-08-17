b26038e79e81c81e0fecd6b004ab93cef9799326
dog-verao <contatoaguiarti@gmail.com>
2020-08-17 15:19:42 +0100

    feat: leaving my local

---
+++ .storybook/preview.js
@@ -0,0 +1,15 @@
+import { addParameters, addDecorator } from "@storybook/react";
+import { withContexts } from "@storybook/addon-contexts/react";
+import { withKnobs } from "@storybook/addon-knobs";
+import { contexts } from "./contexts";
+
+addParameters({
+  background: [
+    { name: "Default theme", value: "#FFFFFF", default: true},
+    { name: "Dark theme", value: "#050449",},
+  ]
+});
+
+
+addDecorator(withContexts(contexts));
+addDecorator(withKnobs);
